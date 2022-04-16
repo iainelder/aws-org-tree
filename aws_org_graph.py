df8e1373a204b3a2d496af0f8ca4f4cfcdade5ca
Iain Samuel McLean Elder <iain@isme.es>
2022-04-16 21:23:59 +0200

    Shorten boto3 type names

--- aws_org_graph.py
+++ aws_org_graph.py
@@ -2,7 +2,11 @@ from queue import SimpleQueue
 from typing import Iterable, Protocol
 
 from boto3 import Session
-from mypy_boto3_organizations.type_defs import AccountTypeDef, OrganizationalUnitTypeDef, RootTypeDef
+from mypy_boto3_organizations.type_defs import (
+    AccountTypeDef as Account,
+    OrganizationalUnitTypeDef as OrgUnit,
+    RootTypeDef as Root,
+)
 from networkx import Graph, freeze  # type: ignore[import]
 
 
@@ -11,31 +15,28 @@ def build_org_graph(session: Session) -> Graph:
     g = Graph()
 
     class GraphWriter:
-        def visit_root(self, root: RootTypeDef) -> None:
+        def visit_root(self, root: Root) -> None:
             g.add_node(root["Id"])
 
-        def visit_account(self, account: AccountTypeDef, parent: OrganizationalUnitTypeDef) -> None:
+        def visit_account(self, account: Account, parent: OrgUnit) -> None:
             g.add_edge(account["Id"], parent["Id"])
 
-        def visit_organizational_unit(self, unit: OrganizationalUnitTypeDef, parent: OrganizationalUnitTypeDef) -> None:
+        def visit_organizational_unit(self, unit: OrgUnit, parent: OrgUnit) -> None:
             g.add_edge(unit["Id"], parent["Id"])
 
-    _traverse_org(Session(), GraphWriter())
+    _traverse_org(session, GraphWriter())
 
     return freeze(g)
 
 
 class Visitor(Protocol):
-
-    def visit_root(self, root: RootTypeDef) -> None:
+    def visit_root(self, root: Root) -> None:
         ...
 
-    def visit_organizational_unit(
-        self, unit: OrganizationalUnitTypeDef, parent: OrganizationalUnitTypeDef
-    ) -> None:
+    def visit_organizational_unit(self, unit: OrgUnit, parent: OrgUnit) -> None:
         ...
 
-    def visit_account(self, account: AccountTypeDef, parent: OrganizationalUnitTypeDef) -> None:
+    def visit_account(self, account: Account, parent: OrgUnit) -> None:
         ...
 
 
@@ -43,7 +44,7 @@ def _traverse_org(session: Session, visitor: Visitor) -> None:
 
     org = session.client("organizations")
 
-    unit_queue: SimpleQueue[OrganizationalUnitTypeDef] = SimpleQueue()
+    unit_queue: SimpleQueue[OrgUnit] = SimpleQueue()
 
     resp = org.list_roots()
 
@@ -64,7 +65,7 @@ def _traverse_org(session: Session, visitor: Visitor) -> None:
             unit_queue.put(unit)
 
 
-def _iter_accounts(session: Session, parent_id: str) -> Iterable[AccountTypeDef]:
+def _iter_accounts(session: Session, parent_id: str) -> Iterable[Account]:
     org = session.client("organizations")
     paginator = org.get_paginator("list_accounts_for_parent")
     for page in paginator.paginate(ParentId=parent_id):
@@ -72,9 +73,7 @@ def _iter_accounts(session: Session, parent_id: str) -> Iterable[AccountTypeDef]
             yield account
 
 
-def _iter_units(
-    session: Session, parent_id: str
-) -> Iterable[OrganizationalUnitTypeDef]:
+def _iter_units(session: Session, parent_id: str) -> Iterable[OrgUnit]:
     org = session.client("organizations")
     paginator = org.get_paginator("list_organizational_units_for_parent")
     for page in paginator.paginate(ParentId=parent_id):
