119fb2a7494832c554dfef9142c5e080a511e7a8
Iain Samuel McLean Elder <iain@isme.es>
2022-04-16 21:20:10 +0200

    Separate org traversal from graph writer

--- aws_org_graph.py
+++ aws_org_graph.py
@@ -1,36 +1,67 @@
 from queue import SimpleQueue
-from typing import Iterable
+from typing import Iterable, Protocol
 
 from boto3 import Session
-from mypy_boto3_organizations.type_defs import AccountTypeDef, OrganizationalUnitTypeDef
+from mypy_boto3_organizations.type_defs import AccountTypeDef, OrganizationalUnitTypeDef, RootTypeDef
 from networkx import Graph, freeze  # type: ignore[import]
 
 
 def build_org_graph(session: Session) -> Graph:
 
     g = Graph()
-    unit_queue: SimpleQueue[OrganizationalUnitTypeDef] = SimpleQueue()
+
+    class GraphWriter:
+        def visit_root(self, root: RootTypeDef) -> None:
+            g.add_node(root["Id"])
+
+        def visit_account(self, account: AccountTypeDef, parent: OrganizationalUnitTypeDef) -> None:
+            g.add_edge(account["Id"], parent["Id"])
+
+        def visit_organizational_unit(self, unit: OrganizationalUnitTypeDef, parent: OrganizationalUnitTypeDef) -> None:
+            g.add_edge(unit["Id"], parent["Id"])
+
+    _traverse_org(Session(), GraphWriter())
+
+    return freeze(g)
+
+
+class Visitor(Protocol):
+
+    def visit_root(self, root: RootTypeDef) -> None:
+        ...
+
+    def visit_organizational_unit(
+        self, unit: OrganizationalUnitTypeDef, parent: OrganizationalUnitTypeDef
+    ) -> None:
+        ...
+
+    def visit_account(self, account: AccountTypeDef, parent: OrganizationalUnitTypeDef) -> None:
+        ...
+
+
+def _traverse_org(session: Session, visitor: Visitor) -> None:
 
     org = session.client("organizations")
+
+    unit_queue: SimpleQueue[OrganizationalUnitTypeDef] = SimpleQueue()
+
     resp = org.list_roots()
 
     for root in resp["Roots"]:
+        visitor.visit_root(root)
         unit_queue.put(root)
 
     while not unit_queue.empty():
         parent = unit_queue.get()
-        g.add_node(parent["Id"])
 
         child_accounts = _iter_accounts(session, parent["Id"])
-        for ac in child_accounts:
-            g.add_edge(parent["Id"], ac["Id"])
+        for account in child_accounts:
+            visitor.visit_account(account, parent)
 
         child_units = _iter_units(session, parent["Id"])
         for unit in child_units:
-            g.add_edge(parent["Id"], unit["Id"])
+            visitor.visit_organizational_unit(unit, parent)
             unit_queue.put(unit)
-
-    return freeze(g)
 
 
 def _iter_accounts(session: Session, parent_id: str) -> Iterable[AccountTypeDef]:
