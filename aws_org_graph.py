9465faa71e362fa20d4df6ac7cb5bbdd5be19680
Iain Samuel McLean Elder <iain@isme.es>
2022-04-16 06:20:33 +0200

    Add tests for empty unit

--- aws_org_graph.py
+++ aws_org_graph.py
@@ -1,26 +1,30 @@
 from boto3 import Session
 from networkx import Graph
-from queue import SimpleQueue, Empty
+from queue import SimpleQueue
 
 def build_org_graph(session: Session) -> Graph:
 
     g = Graph()
-    ouq = SimpleQueue()
+    unit_queue = SimpleQueue()
 
     org = session.client("organizations")
     resp = org.list_roots()
 
     if resp["Roots"]:
         root = resp["Roots"][0]
-        ouq.put(root)
+        unit_queue.put(root)
     
-    while True:
-        try:
-            ou = ouq.get(block=False)
-            child_accounts = org.list_accounts_for_parent(ParentId=ou["Id"])["Accounts"]
-            for ac in child_accounts:
-                g.add_edge(ou["Id"], ac["Id"])
-        except Empty:
-            break
+    while not unit_queue.empty():
+        parent = unit_queue.get(block=False)
+        g.add_node(parent["Id"])
+
+        child_accounts = org.list_accounts_for_parent(ParentId=parent["Id"])["Accounts"]
+        for ac in child_accounts:
+            g.add_edge(parent["Id"], ac["Id"])
+
+        child_units = org.list_organizational_units_for_parent(ParentId=parent["Id"])["OrganizationalUnits"]
+        for unit in child_units:
+            g.add_edge(parent["Id"], unit["Id"])
+            unit_queue.put(unit)
 
     return g
--- test_build_org_tree.py
+++ test_build_org_tree.py
@@ -1,10 +1,32 @@
-from typing import Iterator, Final
+from typing import cast, Iterator, Final
 from boto3 import Session
 from moto import mock_organizations, mock_sts, organizations
 from pytest import fixture, raises, mark, fail
 from aws_org_graph import build_org_graph
 import networkx as nx
 from pytest_mock import MockerFixture
+from mypy_boto3_organizations.type_defs import AccountTypeDef
+from mypy_boto3_organizations.type_defs import OrganizationalUnitTypeDef
+
+
+def _create_org(session: Session) -> None:
+    org = session.client("organizations")
+    org.create_organization(FeatureSet="ALL")
+
+
+def _add_member_account(session: Session) -> AccountTypeDef:
+    org = session.client("organizations")
+    resp = org.create_account(Email="account@example.com", AccountName="Example")
+    new_account_id = resp["CreateAccountStatus"]["AccountId"]
+    member = org.describe_account(AccountId=new_account_id)["Account"]
+    return member
+
+
+def _add_unit(session: Session, parent_id: str) -> OrganizationalUnitTypeDef:
+    org = session.client("organizations")
+    resp = org.create_organizational_unit(ParentId=parent_id, Name="Example")
+    unit = cast(OrganizationalUnitTypeDef, resp["OrganizationalUnit"])
+    return unit
 
 
 ROOT_ID: Final = "r-0001"
@@ -18,21 +40,15 @@ def draw(g: nx.Graph):
 
 
 @fixture
-def session() -> Iterator[Session]:
-    with mock_sts(), mock_organizations():
-        yield Session()
-
-
-@fixture
-def new_org(session: Session, mocker: MockerFixture):
+def session(mocker: MockerFixture) -> Iterator[Session]:
 
     mocker.patch(
         "moto.organizations.utils.make_random_root_id",
         lambda: ROOT_ID
     )
 
-    org = session.client("organizations")
-    org.create_organization(FeatureSet="ALL")
+    with mock_sts(), mock_organizations():
+        yield Session()
 
 
 def test_when_no_org_exists_returns_empty_graph(session: Session):
@@ -41,8 +57,11 @@ def test_when_no_org_exists_returns_empty_graph(session: Session):
     assert nx.utils.graphs_equal(empty, g)
 
 
-@mark.usefixtures("new_org")
-class Test_When_org_is_new:
+class Test_when_org_is_new:
+
+    @fixture(autouse=True)
+    def org(self, session: Session) -> None:
+        _create_org(session)
 
     def test_graph_has_root(self, session: Session):
         g = build_org_graph(session)
@@ -55,3 +74,39 @@ class Test_When_org_is_new:
     def test_graph_has_edge_between_root_and_management_account(self, session: Session):
         g = build_org_graph(session)
         assert (ROOT_ID, MANAGEMENT_ACCOUNT_ID) in g.edges
+
+
+class Test_when_org_has_one_member:
+
+    member: AccountTypeDef
+
+    @fixture(autouse=True)
+    def org(self, session: Session) -> None:
+        _create_org(session)
+        self.member = _add_member_account(session)
+
+    def test_graph_contains_member(self, session: Session):
+        g = build_org_graph(session)
+        assert self.member["Id"] in g
+
+    def test_graph_has_edge_from_member_to_root(self, session: Session):
+        g = build_org_graph(session)
+        assert (ROOT_ID, self.member["Id"]) in g.edges
+
+
+class Test_when_org_has_empty_unit:
+
+    unit: OrganizationalUnitTypeDef
+
+    @fixture(autouse=True)
+    def org(self, session: Session) -> None:
+        _create_org(session)
+        self.unit = _add_unit(session, parent_id=ROOT_ID)
+
+    def test_graph_contains_unit(self, session: Session):
+        g = build_org_graph(session)
+        assert self.unit["Id"] in g
+
+    def test_graph_has_edge_from_unit_to_root(self, session: Session):
+        g = build_org_graph(session)
+        assert (ROOT_ID, self.unit["Id"]) in g.edges
