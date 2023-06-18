979af4cce9d3a9e1f7b930f2c05248fb516793cf
Iain Samuel McLean Elder <iain@isme.es>
2023-06-18 22:12:47 +0200

    Remove junk from tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,16 +1,11 @@
-from io import StringIO
-from typing import Any, Dict, List
-from unittest.mock import Mock, patch
-
-import pytest
-from cli_test_helpers import ArgvContext, shell  # type: ignore[import]
+from cli_test_helpers import shell
 
 
-def test_cli_runs_as_command() -> None:
+def test_runs_as_command() -> None:
     result = shell("aws-org-tree --help")
     assert result.exit_code == 0
 
 
-def test_cli_runs_as_module() -> None:
+def test_runs_as_module() -> None:
     result = shell("python -m aws_org_tree --help")
     assert result.exit_code == 0
