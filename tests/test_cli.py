ac0d898f4a5b6ea50203283dbe1000a5adc06efc
Iain Samuel McLean Elder <iain@isme.es>
2023-06-18 22:02:44 +0200

    Make executable as module

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,3 +9,8 @@ from cli_test_helpers import ArgvContext, shell  # type: ignore[import]
 def test_cli_runs_as_command() -> None:
     result = shell("aws-org-tree --help")
     assert result.exit_code == 0
+
+
+def test_cli_runs_as_module() -> None:
+    result = shell("python -m aws_org_tree --help")
+    assert result.exit_code == 0
