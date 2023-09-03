4797fd17f5690cea4bb778b1ee590783ef55c272
Iain Samuel McLean Elder <iain@isme.es>
2023-09-03 05:35:35 +0200

    Use moto to test tree

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import re
 from unittest.mock import patch
 
 from boto3.session import Session
@@ -9,11 +10,17 @@ from aws_org_tree.aws_org_tree import OrgTree
 
 from typing import Any, Callable, Iterable
 from types import SimpleNamespace
-
+from moto import mock_organizations,  mock_sts
 
 @pytest.fixture()
 def session() -> Iterable[Session]:
-    yield Session(aws_access_key_id="None", aws_secret_access_key="None")
+    with mock_sts(), mock_organizations():
+        yield Session(aws_access_key_id="None", aws_secret_access_key="None")
+
+
+@pytest.fixture()
+def new_organization(session: Session) -> None:
+    session.client("organizations").create_organization(FeatureSet="ALL")
 
 
 def service_error_factory(
@@ -32,8 +39,12 @@ def service_error_factory(
     "botocore.client.BaseClient._make_request",
     service_error_factory("AWSOrganizationsNotInUseException"),
 )
-def test_not_in_use(session: Session) -> None:
-    ot = OrgTree()
+def test_raise_exception_when_org_not_in_use(session: Session) -> None:
     with pytest.raises(ClientError) as excinfo:
-        ot.build()
+        OrgTree(session).build().to_dict()
     assert excinfo.value.response["Error"]["Code"] == "AWSOrganizationsNotInUseException"
+
+@pytest.mark.usefixtures("new_organization")
+def test_tree_has_root_when_org_is_new(session: Session) -> None:
+    tree = OrgTree(session).build().to_dict()
+    assert re.fullmatch(r"r-[a-z0-9]{4}", tree["name"])
